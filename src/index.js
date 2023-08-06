906fbbc505186575859ef1c0b5d54e45a1ec780f
Estelope <elopez14567@gmail.com>
2023-08-06 16:52:21 -0600

    add Ticketmaster API WIP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import TicketMasterEvents from './otherScripts';
 //ui
 
-window.addEventListener("load", function() {
-  console.log("locked and loaded")
+function getEvents(keyWord) {
+  TicketMasterEvents.getEvents(keyWord)
+    .then(function (response) {
+      if (response._embedded) {
+        printElements(response, keyWord);
+      } else {
+        printError(response, keyWord);
+      }
+    });
+}
+
+
+function printElements(response, keyWord) {
+  document.querySelector('#showResponse').innerText = `Results for: ${keyWord} is ${response._embedded.events}.
+  < The temperature in Kelvins is ${response.main} degrees.`;
+}
+
+function printError(error, keyWord) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the event data for ${keyWord}.`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const keyWord = document.querySelector('#keyWord').value;
+  document.querySelector('#keyWord').value = null;
+  getEvents(keyWord);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
+
+
+
+
--- src/otherScripts.js
+++ src/otherScripts.js
@@ -1,2 +1,17 @@
 //business logic
-export default function 
+export default class TicketMasterEvents {
+  static getEvents(keyWord) {
+    return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&keyWord=${keyWord}&apikey=${process.env.API_KEY}`)
+      .then(function (response) {
+        if (!response.ok) {
+          const errorMessage = `${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        return error;
+      });
+}
+}
